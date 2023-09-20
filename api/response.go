1c7c2a9c817009e224a88a2e22ff59f267597759
LQZ-1124 <1248869969@qq.com>
2023-09-20 17:41:06 +0800

    前端增加请求封装

--- api/response.go
+++ api/response.go
@@ -1,54 +1,58 @@
-package api
+package response
 
-import "encoding/json"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/go-admin-team/go-admin-core/sdk/pkg"
+)
 
-type Response struct {
-	Code uint32 `json:"Code"`
-	Msg  string `json:"Msg"`
-	Data any    `json:"Data"`
+var Default = &response{}
+
+// Error 失败数据处理
+func Error(c *gin.Context, code int, err error, msg string) {
+	res := Default.Clone()
+	if err != nil {
+		res.SetMsg(err.Error())
+	}
+	if msg != "" {
+		res.SetMsg(msg)
+	}
+	res.SetTraceID(pkg.GenerateMsgIDFromContext(c))
+	res.SetCode(int32(code))
+	res.SetSuccess(false)
+	c.Set("result", res)
+	c.Set("status", code)
+	c.AbortWithStatusJSON(http.StatusOK, res)
 }
 
-func ResponseMsg(code uint32, msg string, data any) (string, error) {
-	response := Response{
-		Code: code,
-		Msg:  msg,
-		Data: data,
+// OK 通常成功数据处理
+func OK(c *gin.Context, data interface{}, msg string) {
+	res := Default.Clone()
+	res.SetData(data)
+	res.SetSuccess(true)
+	if msg != "" {
+		res.SetMsg(msg)
 	}
-
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		return "", err
-	}
-
-	return string(jsonResponse), nil
+	res.SetTraceID(pkg.GenerateMsgIDFromContext(c))
+	res.SetCode(http.StatusOK)
+	c.Set("result", res)
+	c.Set("status", http.StatusOK)
+	c.AbortWithStatusJSON(http.StatusOK, res)
 }
 
-func ResponseSuccess(msg string) string {
-	response := Response{
-		Code: RSP_SUCCECC,
-		Msg:  msg,
-		Data: nil,
-	}
-
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		return ""
-	}
-
-	return string(jsonResponse)
+// PageOK 分页数据处理
+func PageOK(c *gin.Context, result interface{}, count int, pageIndex int, pageSize int, msg string) {
+	var res page
+	res.List = result
+	res.Count = count
+	res.PageIndex = pageIndex
+	res.PageSize = pageSize
+	OK(c, res, msg)
 }
 
-func ResponseError(msg string) string {
-	response := Response{
-		Code: RSP_ERROR,
-		Msg:  msg,
-		Data: nil,
-	}
-
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		return ""
-	}
-
-	return string(jsonResponse)
+// Custum 兼容函数
+func Custum(c *gin.Context, data gin.H) {
+	data["requestId"] = pkg.GenerateMsgIDFromContext(c)
+	c.Set("result", data)
+	c.AbortWithStatusJSON(http.StatusOK, data)
 }
