70757f3cd8d80048987d8004699a53887d62714e
ryan <ryanwymail@163.com>
2023-10-30 16:26:09 +0800

    更改配置文件路径

--- apiservice/apiservice.go
+++ apiservice/apiservice.go
@@ -12,7 +12,7 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "/Users/ryan/Desktop/development/go/sectran/apiservice/etc/sectran-api.yaml", "the config file")
+var configFile = flag.String("f", "etc/sectran-api.yaml", "the config file")
 
 func main() {
 	flag.Parse()
